4de8db0b1f4809209ae3df06a131a94e887205dc
ck-fm0211 <17825200+ck-fm0211@users.noreply.github.com>
2024-12-29 12:46:06 +0900

    format

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
-from flask import abort, Response
 import os
-from datetime import datetime, timezone, timedelta
+from datetime import datetime, timedelta, timezone
+
 import requests
-import json
+from flask import Response
+
 
 def notify_from_mailbox(request):
-
-    JST = timezone(timedelta(hours=+9), 'JST')
+    JST = timezone(timedelta(hours=+9), "JST")
     dt_now = datetime.now(JST)
 
-    if request.method != 'POST':
-        print(f'This is not post request: {request.method}')
-        return Response(response=f'This is not post request: {request.method}', status=400)
+    if request.method != "POST":
+        print(f"This is not post request: {request.method}")
+        return Response(response=f"This is not post request: {request.method}", status=400)
 
     request_dict = request.get_json()
 
     # バッテリー情報を取得する
-    if request_dict['batteryLevel'] >= 0.5:
-        battery_notification = 'バッテリー残量は充分です'
+    if request_dict["batteryLevel"] >= 0.5:
+        battery_notification = "バッテリー残量は充分です"
     else:
-        battery_notification = 'そろそろバッテリーが切れます'
+        battery_notification = "そろそろバッテリーが切れます"
 
-    notify_message = "ポストに投函がありました。\n" \
-    + f"投函時刻：{dt_now.strftime('%Y年%m月%d日 %H:%M:%S')}\n" \
-    + f"{battery_notification}\n" \
-    + f"<debug> clickType: {request_dict['clickType']}"
+    notify_message = (
+        "ポストに投函がありました。\n"
+        + f"投函時刻：{dt_now.strftime('%Y年%m月%d日 %H:%M:%S')}\n"
+        + f"{battery_notification}\n"
+        + f"<debug> clickType: {request_dict['clickType']}"
+    )
 
     # Slackへ送信
-    slack_payload = {"token": os.environ['SLACK_TOKEN'],
-                    "channel": os.environ['SLACK_CHANNEL'],
-                    "username": "ポスト投函通知",
-                    "text": f"<!channel> {notify_message}"}
+    slack_payload = {
+        "token": os.environ["SLACK_TOKEN"],
+        "channel": os.environ["SLACK_CHANNEL"],
+        "username": "ポスト投函通知",
+        "text": f"<!channel> {notify_message}",
+    }
 
     # slack_res = requests.post('https://slack.com/api/chat.postMessage', data=slack_payload)
 
